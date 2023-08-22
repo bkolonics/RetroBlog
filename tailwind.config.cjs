84c153699a2629aedb3081939efaf32b80a061bc
Bence Kolonics <bence.kolonics@imt-bs.eu>
2023-08-22 02:06:49 +0200

    Added theme for editor & corrected visible backticks

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			typography (theme) {
+        return {
+          DEFAULT: {
+            css: {
+              'code::before': {
+                content: 'none', // don’t generate the pseudo-element
+//                content: '""', // this is an alternative: generate pseudo element using an empty string
+              },
+              'code::after': {
+                content: 'none'
+              },
+              code: {
+                color: theme('colors.gray.700'),
+                backgroundColor: '#E8E3C2',
+                borderRadius: theme('borderRadius.DEFAULT'),
+                paddingLeft: theme('spacing[1.5]'),
+                paddingRight: theme('spacing[1.5]'),
+                paddingTop: theme('spacing.1'),
+                paddingBottom: theme('spacing.1'),
+              },
+            }
+          }
+        }
+      }
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
